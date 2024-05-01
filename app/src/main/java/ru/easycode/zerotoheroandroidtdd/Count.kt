c252de55acd7dcc3fb4d5733556126958d5240f5
Danila <rexerofficial@gmail.com>
2024-05-01 20:26:49 +0300

    task/0012 code polished

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/Count.kt
@@ -0,0 +1,47 @@
+package ru.easycode.zerotoheroandroidtdd
+
+interface Count {
+    fun increment(number: String): UiState
+    fun initial(number: String): UiState
+    fun decrement(number: String): UiState
+
+    class Base(private val step: Int, private val max: Int, private val min: Int) : Count {
+        init {
+            if (step <= 0)
+                throw IllegalStateException("step should be positive, but was $step")
+            if (max <= 0)
+                throw IllegalStateException("max should be positive, but was $max")
+            if (step > max)
+                throw IllegalStateException("max should be more than step")
+            if (max < min)
+                throw IllegalStateException("max should be more than min")
+        }
+
+        override fun increment(number: String): UiState {
+            val result = number.toInt() + step
+            return if (result + step > max)
+                UiState.Max(result.toString())
+            else
+                UiState.Base(result.toString())
+
+        }
+
+        override fun initial(number: String): UiState {
+            return if (number.toInt() - step <= min)
+                UiState.Min(number)
+            else if (number.toInt() == max)
+                UiState.Max(number)
+            else
+                UiState.Base(number)
+        }
+
+        override fun decrement(number: String): UiState {
+            val result = number.toInt() - step
+            return if (result - step < min)
+                UiState.Min(result.toString())
+            else
+                UiState.Base(result.toString())
+        }
+
+    }
+}
--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,59 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var linearLayout: LinearLayout
+    private lateinit var textView: TextView
+    private lateinit var incrementButton: Button
+    private lateinit var decrementButton: Button
+    private val count: Count = Count.Base(2, 4, 0)
+    private var uiState: UiState = count.initial("0")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        linearLayout = findViewById(R.id.rootLayout)
+        textView = findViewById(R.id.countTextView)
+        incrementButton = findViewById(R.id.incrementButton)
+        decrementButton = findViewById(R.id.decrementButton)
+
+        if (savedInstanceState != null)
+            uiState.apply(textView, incrementButton, decrementButton)
+
+        incrementButton.setOnClickListener {
+            uiState = count.increment(textView.text.toString())
+            uiState.apply(textView, incrementButton, decrementButton)
+        }
+        decrementButton.setOnClickListener {
+            uiState = count.decrement(textView.text.toString())
+            uiState.apply(textView, incrementButton, decrementButton)
+        }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putSerializable(KEY, uiState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        uiState = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            savedInstanceState.getSerializable(KEY, UiState::class.java) as UiState
+        } else
+            savedInstanceState.getSerializable(KEY) as UiState
+        uiState.apply(textView, incrementButton, decrementButton)
+    }
+
+    companion object {
+        private const val KEY = "key"
+    }
+}
+
+interface HandleIncrement{
+    fun handle()
 }
