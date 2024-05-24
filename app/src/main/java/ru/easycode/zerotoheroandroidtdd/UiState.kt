9b0d6f7af108ef5eca508b71d04c99f6815b2609
Danila <rexerofficial@gmail.com>
2024-05-24 20:58:18 +0300

    task/0015 Solved

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/UiState.kt
@@ -0,0 +1,30 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+
+interface UiState {
+    fun apply(
+        progressBar: ProgressBar,
+        textView: TextView,
+        button: Button
+    )
+
+    object ShowProgress : UiState{
+        override fun apply(progressBar: ProgressBar, textView: TextView, button: Button) {
+            progressBar.visibility = View.VISIBLE
+            button.isEnabled = false
+            textView.visibility = View.GONE
+        }
+
+    }
+    object ShowData: UiState{
+        override fun apply(progressBar: ProgressBar, textView: TextView, button: Button) {
+            progressBar.visibility = View.GONE
+            textView.visibility = View.VISIBLE
+            button.isEnabled = true
+        }
+    }
+}
