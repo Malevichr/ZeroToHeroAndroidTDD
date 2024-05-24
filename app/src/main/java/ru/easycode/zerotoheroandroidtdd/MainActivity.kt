034e83981ab317519d3dbd3a2df9df0408f2024e
Danila <rexerofficial@gmail.com>
2024-05-24 21:48:13 +0300

    task/0016 Solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,25 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewModel = (application as App).viewModel
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.actionButton.setOnClickListener {
+            viewModel.load()
+        }
+        viewModel.liveData().observe(this) { uiState ->
+            uiState.apply(binding.titleTextView, binding.actionButton, binding.progressBar)
+        }
     }
 }
